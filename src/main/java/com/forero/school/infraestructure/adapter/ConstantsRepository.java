b9b453edbb67a8c1dd8748a853e5a7c7933916d4
Johana Forero Mendoza <johanaforeromendoza31@gmail.com>
2024-08-21 17:00:31 +0200

    "[H-1] Implementation of the Excel note upload service"

--- src/main/java/com/forero/school/infraestructure/adapter/ConstantsRepository.java
+++ src/main/java/com/forero/school/infraestructure/adapter/ConstantsRepository.java
@@ -1,14 +1,37 @@
 package com.forero.school.infraestructure.adapter;
 
+import java.math.BigDecimal;
+
 public final class ConstantsRepository {
     public static final String TITLE = "List of records";
     public static final String RECORDS = "records";
+    public static final String SUBJECTS = "subjects";
     public static final String FILE = "file";
     public static final String REGISTERED = "registered";
     public static final String NOTE = "note";
+    public static final BigDecimal TOTAL_NOTES = BigDecimal.valueOf(3);
     public static final String CONCATENATE = "_";
     public static final String DEFAULT_VALUE = "";
     public static final float TABLE_WIDTH = 500.0f;
+    public static final int MINI_VALOR = 0;
+    public static final int NOTE_ONE_CELL_INDEX = 1;
+    public static final int NOTE_TWO_CELL_INDEX = 2;
+    public static final int NOTE_THREE_CELL_INDEX = 3;
+    public static final BigDecimal MAX_NOTE = BigDecimal.valueOf(100);
+    public static final float HEADER_MARGIN_LEFT = 50.0f;
+    public static final float SPACE_CELLS = 1;
+    public static final float MARGIN_TOP = 680;
+    public static final int DECIMAL_PRECISION = 2;
+    public static final float MARGIN_TOP_MIN = 50;
+    public static final int FRONT_SIZE = 10;
+    public static final int ROW_HEIGHT = 20;
+    public static final float HEADER_MARGIN_TOP = 700.0f;
+    public static final float HEADER_FONT_SIZE = 12.0f;
+    public static final int TITLE_FONT_SIZE = 16;
+    public static final float TITLE_X_OFFSET = 220f;
+    public static final float TITLE_Y_OFFSET = 750f;
+    public static final float DOCUMENT_CELL_WIDTH_ADJUSTMENT_FACTOR = 1.01f;
+
     static final String[] HEADERS = {"Student", "Name", "Surname", "Document", "Note1", "Note2", "Note3",
             "Average"};
 
--- src/main/java/com/forero/school/infraestructure/adapter/MySQLRepositoryServiceImpl.java
+++ src/main/java/com/forero/school/infraestructure/adapter/MySQLRepositoryServiceImpl.java
@@ -60,6 +60,7 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
     public List<GeneralAggregate<StudentEntity>> getAllSubjectAndStudents() {
         final List<GeneralAggregate<StudentEntity>> result = new ArrayList<>();
         final List<SubjectEntity> subjects = this.subjectRepository.findAll();
+        this.validateSubjectEntityList(subjects);
         for (final SubjectEntity subject : subjects) {
             final GeneralAggregate<StudentEntity> generalAggregate = new GeneralAggregate<>();
             final List<StudentEntity> students = this.getStudentsBySubjectId(subject.getId().intValue());
@@ -82,9 +83,7 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
     public List<Registered> getAllRegistered(final int subjectId) {
         final List<RegisteredEntity> registeredEntityList =
                 this.registeredRepository.findAllBySubjectIdOrderByAverageDesc((long) subjectId);
-        if (registeredEntityList.isEmpty()) {
-            throw new RepositoryException(CodeException.EMPTY_LIST, null, ConstantsRepository.RECORDS);
-        }
+        this.validateRegisteredEntityList(registeredEntityList);
         return registeredEntityList
                 .stream()
                 .map(this.registeredMapper::toEntityToModel)
@@ -94,15 +93,16 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
     @Override
     @Transactional
     public void uploadGrades(final MultipartFile[] files, final int idSubject) {
-        final MultipartFile file = files[0];
+        final MultipartFile file = files[ConstantsRepository.MINI_VALOR];
         try (final InputStream is = file.getInputStream();
              final Workbook workbook = new XSSFWorkbook(is)) {
 
-            final Sheet sheet = workbook.getSheetAt(0);
+            final Sheet sheet = workbook.getSheetAt(ConstantsRepository.MINI_VALOR);
             final Iterator<Row> iterator = sheet.iterator();
 
             if (!iterator.hasNext()) {
-                throw new RepositoryException(CodeException.INVALID_PARAMETERS, null, ConstantsRepository.FILE);
+                throw new RepositoryException(CodeException.INVALID_PARAMETERS, null,
+                        ConstantsRepository.FILE);
             }
 
             iterator.next();
@@ -122,7 +122,8 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
 
             while (iterator.hasNext()) {
                 final Row row = iterator.next();
-                final String studentIdentification = this.getStudentIdentification(row.getCell(0));
+                final String studentIdentification = this.getStudentIdentification(
+                        row.getCell(ConstantsRepository.MINI_VALOR));
 
                 if (studentIdentifications.contains(studentIdentification)) {
                     throw new RepositoryException(CodeException.DUPLICATE_STUDENT_IN_EXCEL, null,
@@ -131,9 +132,12 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
 
                 studentIdentifications.add(studentIdentification);
 
-                final BigDecimal noteOne = this.getBigDecimalFromCell(row.getCell(1));
-                final BigDecimal noteTwo = this.getBigDecimalFromCell(row.getCell(2));
-                final BigDecimal noteThree = this.getBigDecimalFromCell(row.getCell(3));
+                final BigDecimal noteOne = this.getBigDecimalFromCell(row.getCell(
+                        ConstantsRepository.NOTE_ONE_CELL_INDEX));
+                final BigDecimal noteTwo = this.getBigDecimalFromCell(row.getCell(
+                        ConstantsRepository.NOTE_TWO_CELL_INDEX));
+                final BigDecimal noteThree = this.getBigDecimalFromCell(row.getCell(
+                        ConstantsRepository.NOTE_THREE_CELL_INDEX));
                 this.validateNotes(noteOne, noteTwo, noteThree);
 
                 final RegisteredEntity existingRegistration = existingRegistrations.get(
@@ -176,15 +180,16 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
             throw new RepositoryException(CodeException.INVALID_PARAMETERS, null, ConstantsRepository.NOTE);
         }
 
-        if (note.compareTo(BigDecimal.ONE) < 0 || note.compareTo(BigDecimal.valueOf(100)) > 0) {
+        if (note.compareTo(BigDecimal.ONE) < ConstantsRepository.MINI_VALOR ||
+                note.compareTo(ConstantsRepository.MAX_NOTE) > ConstantsRepository.MINI_VALOR) {
             throw new RepositoryException(CodeException.INVALID_NOTE, null);
         }
     }
 
     private BigDecimal calculateAverage(final BigDecimal noteOne, final BigDecimal noteTwo, final BigDecimal noteThree) {
         final BigDecimal summationNotes = noteOne.add(noteTwo).add(noteThree);
-        final BigDecimal totalNotes = BigDecimal.valueOf(3);
-        return summationNotes.divide(totalNotes, 2, RoundingMode.HALF_EVEN);
+        final BigDecimal totalNotes = ConstantsRepository.TOTAL_NOTES;
+        return summationNotes.divide(totalNotes, ConstantsRepository.DECIMAL_PRECISION, RoundingMode.HALF_EVEN);
     }
 
     private BigDecimal getBigDecimalFromCell(final Cell cell) {
@@ -193,7 +198,8 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
         }
 
         final BigDecimal note = BigDecimal.valueOf(cell.getNumericCellValue());
-        if (note.compareTo(BigDecimal.ONE) < 0 || note.compareTo(BigDecimal.valueOf(100)) > 0) {
+        if (note.compareTo(BigDecimal.ONE) < ConstantsRepository.MINI_VALOR || note.compareTo(
+                ConstantsRepository.MAX_NOTE) > ConstantsRepository.MINI_VALOR) {
             throw new RepositoryException(CodeException.INVALID_NOTE, null);
         }
 
@@ -221,6 +227,12 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
         }
     }
 
+    private void validateSubjectEntityList(final List<SubjectEntity> subjectEntities) {
+        if (subjectEntities.isEmpty()) {
+            throw new RepositoryException(CodeException.EMPTY_LIST, null, ConstantsRepository.SUBJECTS);
+        }
+    }
+
     private void createPdfContent(final PDDocument document, final List<RegisteredEntity> registeredEntityList)
             throws IOException {
         final PDPage page = new PDPage();
@@ -234,26 +246,26 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
     }
 
     private void createTitle(final PDPageContentStream contentStream) throws IOException {
-        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 16);
+        contentStream.setFont(PDType1Font.HELVETICA_BOLD, ConstantsRepository.TITLE_FONT_SIZE);
         contentStream.beginText();
-        contentStream.newLineAtOffset(220, 750);
+        contentStream.newLineAtOffset(ConstantsRepository.TITLE_X_OFFSET, ConstantsRepository.TITLE_Y_OFFSET);
         contentStream.showText(ConstantsRepository.TITLE);
         contentStream.endText();
     }
 
     private void createHeader(final PDPageContentStream contentStream) throws IOException {
-        final float marginLeft = 50;
-        final float marginTop = 700;
-        final float tableWidth = 500;
+        final float marginLeft = ConstantsRepository.HEADER_MARGIN_LEFT;
+        final float marginTop = ConstantsRepository.HEADER_MARGIN_TOP;
+        final float tableWidth = ConstantsRepository.TABLE_WIDTH;
         final String[] headers = ConstantsRepository.HEADERS;
 
-        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+        contentStream.setFont(PDType1Font.HELVETICA_BOLD, ConstantsRepository.HEADER_FONT_SIZE);
         contentStream.beginText();
         contentStream.newLineAtOffset(marginLeft, marginTop);
 
         for (final String header : headers) {
             contentStream.showText(header);
-            contentStream.newLineAtOffset(tableWidth / headers.length, 0);
+            contentStream.newLineAtOffset(tableWidth / headers.length, ConstantsRepository.MINI_VALOR);
         }
 
         contentStream.endText();
@@ -261,16 +273,16 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
 
     private void createBody(final PDDocument document, PDPageContentStream contentStream,
                             final List<RegisteredEntity> registeredEntityList) throws IOException {
-        float marginTop = 680;
-        final float rowHeight = 20;
+        float marginTop = ConstantsRepository.MARGIN_TOP;
+        final float rowHeight = ConstantsRepository.ROW_HEIGHT;
 
-        contentStream.setFont(PDType1Font.HELVETICA, 10);
+        contentStream.setFont(PDType1Font.HELVETICA, ConstantsRepository.FRONT_SIZE);
 
         for (final RegisteredEntity registered : registeredEntityList) {
-            if (marginTop < 50) {
+            if (marginTop < ConstantsRepository.MARGIN_TOP_MIN) {
                 contentStream.close();
                 contentStream = this.createNewPage(document);
-                marginTop = 680;
+                marginTop = ConstantsRepository.MARGIN_TOP;
             }
 
             this.drawRow(contentStream, registered, marginTop);
@@ -292,34 +304,35 @@ public class MySQLRepositoryServiceImpl implements RepositoryService {
 
     private void drawRow(final PDPageContentStream contentStream, final RegisteredEntity registered,
                          final float marginTop) throws IOException {
-        final float marginLeft = 50.0f;
+        final float marginLeft = ConstantsRepository.HEADER_MARGIN_LEFT;
         final float cellWidth = ConstantsRepository.TABLE_WIDTH / ConstantsRepository.HEADERS.length;
-        final float increasedCellWidthForDocument = (float) (cellWidth * 1.01);
-        final float spaceBetweenCells = 1;
+        final float increasedCellWidthForDocument = (cellWidth * ConstantsRepository.DOCUMENT_CELL_WIDTH_ADJUSTMENT_FACTOR);
+        final float spaceBetweenCells = ConstantsRepository.SPACE_CELLS;
 
         contentStream.beginText();
         contentStream.newLineAtOffset(marginLeft, marginTop);
 
         contentStream.showText(String.valueOf(registered.getStudent().getId()));
-        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, 0);
+        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, ConstantsRepository.MINI_VALOR);
 
         contentStream.showText(String.valueOf(registered.getStudent().getFirstName()));
-        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, 0);
+        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, ConstantsRepository.MINI_VALOR);
 
         contentStream.showText(String.valueOf(registered.getStudent().getSecondName()));
-        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, 0);
+        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, ConstantsRepository.MINI_VALOR);
 
         contentStream.showText(registered.getStudent().getDocumentNumber());
-        contentStream.newLineAtOffset(increasedCellWidthForDocument + spaceBetweenCells, 0);
+        contentStream.newLineAtOffset(increasedCellWidthForDocument + spaceBetweenCells,
+                ConstantsRepository.MINI_VALOR);
 
         contentStream.showText(String.valueOf(registered.getNota2()));
-        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, 0);
+        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, ConstantsRepository.MINI_VALOR);
 
         contentStream.showText(String.valueOf(registered.getNota3()));
-        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, 0);
+        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, ConstantsRepository.MINI_VALOR);
 
         contentStream.showText(String.valueOf(registered.getNota1()));
-        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, 0);
+        contentStream.newLineAtOffset(cellWidth + spaceBetweenCells, ConstantsRepository.MINI_VALOR);
 
 
         contentStream.showText(String.valueOf(registered.getAverage()));
